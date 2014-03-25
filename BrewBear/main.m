095126d58e4bb93e58b0dfbc619c5889f04356bf
Jacob Howcroft <jhowcrof@umich.edu>
2014-03-25 18:46:53 -0400

    Initial Commit

---
+++ BrewBear/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BrewBear
+//
+//  Created by Jacob Howcroft on 3/25/14.
+//  Copyright (c) 2014 Jacob Howcroft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
